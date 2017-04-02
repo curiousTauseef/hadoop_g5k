996f7bf5f2eb3bf10ef1ad40a6b09c5bd95fdddf
Ophir LOJKINE <pere.jobs@gmail.com>
2017-04-03 00:05:09 +0300

    fix syntax

--- hadoop_g5k/cluster.py
+++ hadoop_g5k/cluster.py
@@ -173,11 +173,11 @@ class HadoopCluster(object):
         self.java_home = get_java_home(self.master)
 
         # 1. Copy hadoop tar file and uncompress
-        rm_command = "rm -rf " + self.base_dir +
+        rm_command = ("rm -rf " + self.base_dir +
                                " " + self.conf_dir +
                                " " + self.logs_dir +
                                " " + self.hadoop_temp_dir,
-                               self.hosts
+                               self.hosts)
         rm_dirs = TaktukRemote(rm_command)
         logger.info("Cleaning target: " + rm_command)
         rm_dirs.run()
